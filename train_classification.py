37dabd01ae18a9a5b6183bffbda613b661c6af67
Minh Hung <elias.mhung@gmail.com>
2026-04-09 18:56:57 +0800

    update

--- train_classification.py
+++ train_classification.py
@@ -22,17 +22,17 @@ class EarlyStopping:
     def __init__(self, patience=15, min_delta=1e-3):
         self.patience = patience
         self.min_delta = min_delta
-        self.best_loss = None
+        self.best_acc = None
         self.counter = 0
         self.early_stop = False
 
-    def __call__(self, val_loss):
-        if self.best_loss is None:
-            self.best_loss = val_loss
+    def __call__(self, val_jersey_n_acc):
+        if self.best_acc is None:
+            self.best_acc = val_jersey_n_acc
             return
 
-        if val_loss < self.best_loss - self.min_delta:
-            self.best_loss = val_loss
+        if val_jersey_n_acc > self.best_acc - self.min_delta:
+            self.best_acc = val_jersey_n_acc
             self.counter = 0
         else:
             self.counter += 1
@@ -441,7 +441,7 @@ if __name__ == '__main__':
 
         plt.close(fig)
 
-        early_stopping(avg_val_loss)
+        early_stopping(best_acc)
         if early_stopping.early_stop:
             print(f"Early stopping at epoch {epoch + 1}")
             break
